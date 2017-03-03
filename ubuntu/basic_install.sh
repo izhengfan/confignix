a229d6e14601bb6cb921522d42850b1cd3718a84
izhengfan <mrzhengfan@gmail.com>
2017-03-03 14:54:35 +0800

    add ubuntu setup

---
+++ ubuntu/basic_install.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install gcc g++  build-essential cmake vim git python-pip python-scipy pkg-config libeigen3-dev libboost-all-dev 
---
+++ ubuntu/remove_useless.sh
@@ -0,0 +1,3 @@
+sudo apt-get remove libreoffice* thunderbird unity-webapps*
+sudo apt-get autoremove
+gsettings set com.canonical.Unity.Lenses remote-content-search 'none'
