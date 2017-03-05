dee0756fefde9ba74c02b69d0536cd29c86172fd
ZHENG Fan 郑帆 <fzheng@mae.cuhk.edu.hk>
2017-03-05 23:31:18 +0800

    Update basic_install.sh

--- ubuntu/basic_install.sh
+++ ubuntu/basic_install.sh
@@ -1,2 +1,4 @@
 sudo apt-get update
-sudo apt-get install gcc g++  build-essential cmake vim git python-pip python-scipy pkg-config libeigen3-dev libboost-all-dev 
+sudo apt-get install gcc g++  build-essential cmake vim git python-pip pkg-config libeigen3-dev libboost-all-dev 
+pip install --upgrade pip
+pip install numpy scipy matplotlib
