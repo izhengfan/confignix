8100fd85e93e7c4d333421d57be891935b886554
ZHENG Fan 郑帆 <fzheng@mae.cuhk.edu.hk>
2017-03-05 14:28:31 +0800

    add install vim-nox-py2

--- vim/autosetup.sh
+++ vim/autosetup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
+sudo apt-get install vim-nox-py2 # required by some new distribution for py2
