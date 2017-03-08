4bb78df443c967660f84d385c7002859612b569b
izhengfan <mrzhengfan@gmail.com>
2017-03-08 12:01:19 +0800

    seperate python

---
+++ python/basic_install.sh
@@ -0,0 +1,2 @@
+pip install --upgrade pip
+pip install numpy scipy matplotlib sympy ptpython ipython jupyter
--- ubuntu/basic_install.sh
+++ ubuntu/basic_install.sh
@@ -1,4 +1,2 @@
 sudo apt-get update
 sudo apt-get install gcc g++  build-essential cmake vim git python-pip pkg-config libeigen3-dev libboost-all-dev 
-pip install --upgrade pip
-pip install numpy scipy matplotlib
