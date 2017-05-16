9d732c59bfd3ee61c3066490a5ae4d09699231cf
izhengfan <mrzhengfan@gmail.com>
2017-05-16 14:27:15 +0800

    fix get-oh-my-zsh

--- zsh/get-oh-my-zsh.sh
+++ zsh/get-oh-my-zsh.sh
@@ -1,4 +1,4 @@
-h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
 
