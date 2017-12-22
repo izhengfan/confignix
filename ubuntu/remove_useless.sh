b9a476399e3a7383f74b6f90d702e3dfb98e79b1
izhengfan <mrzhengfan@gmail.com>
2017-12-22 21:04:44 +0800

    add more pkgs in remove_useless for ubuntu

--- ubuntu/remove_useless.sh
+++ ubuntu/remove_useless.sh
@@ -1,3 +1,3 @@
-sudo apt-get remove libreoffice* thunderbird unity-webapps*
+sudo apt-get remove libreoffice-writer libreoffice-impress libreoffice-calc libreoffice-math thunderbird unity-webapps* gnome-mahjongg gnome-sudoku gnome-maps aisleriot totem cheese empathy gnome-orca
 sudo apt-get autoremove
 gsettings set com.canonical.Unity.Lenses remote-content-search 'none'
