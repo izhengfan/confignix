dfb669fdaf08134b2f82ae93b555fdbe2e7f0cbe
izhengfan <mrzhengfan@gmail.com>
2017-12-24 15:19:31 +0800

    update pip source

--- python/basic_install.sh
+++ python/basic_install.sh
@@ -1,2 +1,9 @@
-pip install --upgrade pip
-pip install numpy scipy matplotlib sympy ptpython pyzmq ipython jupyter
+wget https://bootstrap.pypa.io/get-pip.py
+chmod +x get-pip.py
+
+pip2 install --user numpy scipy matplotlib ipython 
+# pip2 install --user sympy ptipython opencv-python
+pip3 install --user numpy scipy matplotlib ipython
+# pip3 install --user sympy ptipython opencv-python
+
+rm get-pip.py*
