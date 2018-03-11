bb054cf95d05300db59342e40ff3512c778021e7
izhengfan <mrzhengfan@gmail.com>
2018-03-11 12:57:20 +0800

    add grip

--- python/basic_install.sh
+++ python/basic_install.sh
@@ -1,9 +1,9 @@
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python2 get-pip.py
 sudo python3 get-pip.py
-pip2 install --user numpy scipy matplotlib ipython 
+pip2 install --user numpy scipy matplotlib ipython grip
 # pip2 install --user sympy ptipython opencv-python
-pip3 install --user numpy scipy matplotlib ipython
+pip3 install --user numpy scipy matplotlib ipython grip
 # pip3 install --user sympy ptipython opencv-python
 
 rm get-pip.py*
