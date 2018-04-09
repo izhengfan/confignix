ecbdad74c039bc933ef705e00cbb4139a54c1a0c
izhengfan <mrzhengfan@gmail.com>
2018-04-09 16:41:26 +0800

    add customize.sh

---
+++ ubuntu/customize.sh
@@ -0,0 +1,12 @@
+sudo apt install numix-gtk-theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Numix'
+
+# Below set fonts; optional.
+# gsettings set org.gnome.desktop.interface document-font-name 'Roboto 11'
+# gsettings set org.gnome.desktop.interface font-name 'Roboto 11'
+# gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka Term 13'
+
+# Below install Paper theme; optional.
+# sudo add-apt-repository ppa:snwh/pulp
+# sudo apt update
+# sudo apt install paper-gtk-theme
