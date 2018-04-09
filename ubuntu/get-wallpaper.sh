0225cab7acb6ceaf84476f178e61b41c5825e8c8
izhengfan <mrzhengfan@gmail.com>
2018-04-09 17:22:08 +0800

    get wallpapers

---
+++ ubuntu/get-wallpaper.sh
@@ -0,0 +1,3 @@
+# Install some wallpapers I like
+wget "http://i01.appmifile.com/webfile/globalimg/zh/goods/headphonesprohd/earphonepro-zt2.jpg" -O ~/Pictures/sea-waves.jpg
+wget "https://wiki.ubuntu.com/Xubuntu/Roadmap/Specifications/Trusty/CommunityWallpapers/Winners?action=AttachFile&do=get&target=adrianfelipepera_traslasierra.jpg" -O ~/Pictures/traslasierra.jpg
