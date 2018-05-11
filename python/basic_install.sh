7cab298c175817b7039bf29076de6d27c11d06b0
izhengfan <mrzhengfan@gmail.com>
2018-05-11 10:14:44 +0800

    nah, ignore python2

--- python/basic_install.sh
+++ python/basic_install.sh
@@ -1,7 +1,7 @@
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python2 get-pip.py
 sudo python3 get-pip.py
-pip2 install --user numpy scipy matplotlib ipython
+# pip2 install --user numpy scipy matplotlib ipython
 # pip2 install --user sympy ptipython opencv-python
 pip3 install --user numpy scipy matplotlib ipython grip gdbgui
 # pip3 install --user sympy ptipython opencv-python
