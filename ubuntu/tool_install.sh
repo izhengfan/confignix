eb56c143fc5b19bb14e2448e6faf2dc643bdb8fb
izhengfan <hi@fzheng.me>
2019-04-02 12:43:57 +0800

    install ranger

--- ubuntu/tool_install.sh
+++ ubuntu/tool_install.sh
@@ -1,3 +1,4 @@
 sudo apt install chromium-browser vim-gtk git-gui xclip pandoc
 sudo apt install psensor redshift-gtk
 sudo apt install gimp ffmpeg vlc recordmydesktop optipng
+sudo apt install ranger
