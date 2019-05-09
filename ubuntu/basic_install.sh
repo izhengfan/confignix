3396f0138e3fed822cef2ff79361330cedd9617e
izhengfan <hi@fzheng.me>
2019-05-09 18:52:41 +0800

    install net-tools

--- ubuntu/basic_install.sh
+++ ubuntu/basic_install.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install gcc g++  build-essential cmake wget curl vim git python3 libpython3-dev pkg-config libeigen3-dev libboost-all-dev gfortran
+sudo apt-get install gcc g++  build-essential cmake wget curl vim git python3 libpython3-dev pkg-config libeigen3-dev libboost-all-dev gfortran net-tools
