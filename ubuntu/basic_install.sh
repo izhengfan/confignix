e097ac4f257fdd1e27f6ab22b622e2f6a1437f81
izhengfan <hi@fzheng.me>
2019-09-30 13:14:36 +0800

    update apt install

--- ubuntu/basic_install.sh
+++ ubuntu/basic_install.sh
@@ -1,2 +1,3 @@
-sudo apt-get update
-sudo apt-get install gcc g++  build-essential cmake wget curl vim git python3 libpython3-dev pkg-config libeigen3-dev libboost-all-dev gfortran net-tools
+sudo apt update
+sudo apt install gcc g++  build-essential cmake wget curl vim git python3 libpython3-dev pkg-config libeigen3-dev libboost-all-dev gfortran net-tools
+sudo apt install chrome-gnome-shell
