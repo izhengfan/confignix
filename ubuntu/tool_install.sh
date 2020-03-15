2db24ac9de4321945a7dffaf85652de77cf401c9
izhengfan <hi@fzheng.me>
2020-03-15 16:47:02 +0800

    add copyq

--- ubuntu/tool_install.sh
+++ ubuntu/tool_install.sh
@@ -1,4 +1,5 @@
 sudo apt install chromium-browser vim-gtk git-gui xclip pandoc
-sudo apt install psensor redshift-gtk
+# sudo apt install psensor redshift-gtk
 sudo apt install gimp ffmpeg vlc recordmydesktop optipng
+sudo apt install copyq
 sudo apt install ranger
